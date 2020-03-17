208d9a7cf1f5e5e5be6bbc864876723feed7fc36
nick chen <nick.chen.pro@gmail.com>
2020-03-17 17:05:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function breakOut(array, changeValue, stopValue) {
   for (let i = 0; i < array.length; i++) {
+    let array[i] === changeValue;
     if (array[i] === stopValue) {
       break;
     }
